b40988f54f5d39113de37233febaebeed9c5b6e8
Ayaka Hanaoka <aya.yb.0427@gmail.com>
2019-05-14 15:16:01 +0900

    7.3.1/ユーザー登録の成功、失敗時の処理の場合分け

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,13 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      #保存の成功をここで扱う
+    else
+      render 'new'
+    end
+  end
 end
