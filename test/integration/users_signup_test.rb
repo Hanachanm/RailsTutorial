caae58901c1f6bba3f865b9a65cac2b48dc314fc
Ayaka Hanaoka <aya.yb.0427@gmail.com>
2019-05-14 17:06:43 +0900

    7.4.4/ユーザー登録成功時のテスト作成

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,14 +4,26 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, params: { user: { name:  "",
+      post users_path, params: { user: { name: "",
                                         email: "user@invalid",
-                                        password:              "foo",
+                                        password: "foo",
                                         password_confirmation: "bar" } }
     end
     assert_template 'users/new'
     assert_select 'div#error_explanation'
     assert_select 'div.alert'
     assert_select 'form[action="/signup"]'
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: "Example Use",
+                                        email: "user@example.com",
+                                        password: "password",
+                                        password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
   end
 end
