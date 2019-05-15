c4944ddea6bfdad5cee5bb6e6df1eb132058cc16
Ayaka Hanaoka <aya.yb.0427@gmail.com>
2019-05-15 17:42:10 +0900

    is_logged_in?のrubocpo対策

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,4 @@
 module SessionsHelper
-
   # 渡されたユーザーでログインする
   def log_in(user)
     session[:user_id] = user.id
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
-
   def setup
     @user = users(:michael)
   end
