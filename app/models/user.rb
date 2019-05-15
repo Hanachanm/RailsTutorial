6a3cb9576114dce80ee94ac6ba570d79439906eb
Ayaka Hanaoka <aya.yb.0427@gmail.com>
2019-05-15 17:17:29 +0900

    8.2.4/レイアウトの変更のテスト追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,8 @@ class User < ApplicationRecord
 
   # 渡された文字列のハッシュ値を返す
   def User.digest(string)
-    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :　BCrypt::Engine.cost
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
 end
