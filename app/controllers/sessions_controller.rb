ce58139da79a1d7a872ddd7a1ff50b526b5b357c
Ayaka Hanaoka <aya.yb.0427@gmail.com>
2019-05-15 15:52:05 +0900

    8.1.4/フラッシュ追加（暫定）

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(emial: params[:session][:email].downcase)
+    user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
     else
-      # エラーメッセージを作成する
+      flash[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
