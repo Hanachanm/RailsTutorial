2e775fe6904bb87fc72e544235fc4eb24792195f
Ayaka Hanaoka <aya.yb.0427@gmail.com>
2019-05-22 13:33:22 +0900

    11.3.1/authenticated?メソッドの抽象化

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,10 +27,11 @@ class User < ApplicationRecord
   end
 
   # 渡されたトークンがダイジェストと一致したらtrueを返す
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
 
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
   # ユーザーのログイン情報を破棄する
