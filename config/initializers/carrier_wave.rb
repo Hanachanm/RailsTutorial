fee039fc1964c54831e52eb7b078f8f9b8793a26
Ayaka Hanaoka <aya.yb.0427@gmail.com>
2019-05-28 10:21:45 +0900

    13.4.4/carrie_waveの設定

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,7 +3,7 @@ if Rails.env.production?
     config.fog_credentials = {
       # Amazon S3用の設定
       :provider              => 'AWS',
-      :region                => ENV['S3_REGION'],     # 例: 'ap-northeast-1'
+      :region                => ENV['S3_REGION'],
       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
